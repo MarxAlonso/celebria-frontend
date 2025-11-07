6eeff7aadde6a0097259d999e4d9eed5fa23856b
Marx Alonso <marxchip99@gmail.com>
2025-11-07 14:31:45 -0500

    pruebas registro backend

--- lib/api.ts
+++ lib/api.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 
 export const api = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: API_BASE_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 // Interceptor para agregar token a las peticiones
 api.interceptors.request.use((config) => {
